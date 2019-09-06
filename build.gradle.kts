2f4a1af348e5a425de28cd81354586d9c1217341
fedor <fedor.korotkov@gmail.com>
2019-09-06 00:27:14 -0400

    Setup Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.3.41"
+    `java-library`
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    api("io.kubernetes", "client-java", "5.0.0")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
